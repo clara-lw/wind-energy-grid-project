2024a6e1ba24811704cdc3654d9830bc888b828d
clara-lw <ucaplat@ucl.ac.uk>
2026-03-04 22:57:46 +0000

    add methods to plot maps

---
+++ wind_grid/uk_plot.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.colors as mcolors
+
+def plot_map_basic():
+    fig = plt.figure(figsize=(9,10))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+
+    ax.add_feature(cfeature.LAND)
+    ax.add_feature(cfeature.OCEAN)
+    ax.add_feature(cfeature.COASTLINE, linewidth=0.8)
+    ax.add_feature(cfeature.BORDERS, linewidth=0.4)
+
+    ax.set_extent([-9, 3, 49, 61], crs=ccrs.PlateCarree())
+
+    gl = ax.gridlines(draw_labels=True, linewidth=0.3, alpha=0.6)
+    gl.top_labels = False
+    gl.right_labels = False
+
+    return fig, ax
+
+
+
+def plot_map_log_stations(cap, lons, lats):
+    fig, ax = plot_map_basic()
+    sizes = 20 + 180 * (np.sqrt(cap) / np.sqrt(np.nanmax(cap) if np.nanmax(cap) > 0 else 1))
+
+    norm = mcolors.LogNorm(
+    vmin=np.nanmin(cap[cap > 0]),  
+    vmax=np.nanmax(cap)
+    )
+
+    sc = ax.scatter(
+        lons, lats,
+        s=sizes,
+        c=cap,               
+        cmap='viridis',
+        norm=norm,
+        alpha=0.75,
+        edgecolors='k',
+        linewidth=0.2,
+        transform=ccrs.PlateCarree()
+    )
+
+    cbar = plt.colorbar(sc, ax=ax, orientation='vertical', pad=0.02, shrink=0.7)
+    cbar.set_label('Installed Capacity (MW)', fontsize=10)
+
+    return fig, ax
